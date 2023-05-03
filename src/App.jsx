d662c69d0cc54f9e8e84beff774b3bbe102b5e79
Himanshu Rane <himanshurane6501@gmail.com>
2023-05-03 20:50:28 +0530

    Redux Store Satup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  const dipatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/upcoming").then((res) => {
+      console.log(res);
+      dipatch(getApiConfiguration(res));
+    });
+  };
+  return <div>{url?.total_pages}</div>;
 }
 
 export default App;
